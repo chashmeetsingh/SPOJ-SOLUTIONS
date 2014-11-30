6c4d5e4ceffd064625f28ec91d9bc1f47f17aae3
chashmeetsingh <chashmeetsingh@gmail.com>
2014-11-30 23:06:28 +0530

    Create CLOCK2.cpp

---
+++ CLOCK2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+	string n;
+	int i=1;
+	while(cin>>n){
+		if(n=="I"){
+			cout<<"Case "<<i<<": "<<1<<endl;
+		}
+		if(n=="II"){
+			cout<<"Case "<<i<<": "<<2<<endl;
+		}
+		if(n=="III"){
+			cout<<"Case "<<i<<": "<<3<<endl;
+		}
+		if(n=="IV"){
+			cout<<"Case "<<i<<": "<<4<<endl;
+		}
+		if(n=="V"){
+			cout<<"Case "<<i<<": "<<5<<endl;
+		}
+		if(n=="VI"){
+			cout<<"Case "<<i<<": "<<6<<endl;
+		}
+		if(n=="VII"){
+			cout<<"Case "<<i<<": "<<7<<endl;
+		}
+		if(n=="VIII"){
+			cout<<"Case "<<i<<": "<<8<<endl;
+		}
+		if(n=="IX"){
+			cout<<"Case "<<i<<": "<<9<<endl;
+		}
+		if(n=="X"){
+			cout<<"Case "<<i<<": "<<10<<endl;
+		}
+		if(n=="XI"){
+			cout<<"Case "<<i<<": "<<11<<endl;
+		}
+		if(n=="XII"){
+			cout<<"Case "<<i<<": "<<12<<endl;
+		}
+		i++;
+	}
+	return 0;
+}
