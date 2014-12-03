03550492f0f6a7f42a7fea2aee29c3909ea1b263
chashmeetsingh <chashmeetsingh@gmail.com>
2014-12-03 22:03:30 +0530

    Update DOTAA.cpp

--- DOTAA.cpp
+++ DOTAA.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 int main()
 {
-    int t,n,i,m,d,h[100001],c=0;
+    int t,n,i,m,d,a[100001],c=0;
     scanf("%d",&t);
     while(t--)
     {
@@ -10,8 +10,8 @@ int main()
 
         for(i=0;i<n;i++)
             {
-                scanf("%d",&h[i]);
-                while((h[i]-=d)>0) {
+                scanf("%d",&a[i]);
+                while((a[i]-=d)>0) {
                 c++;
             }
             }
