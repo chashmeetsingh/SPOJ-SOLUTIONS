ec030bf3f18df824164c965a0034a09e86011f17
chashmeetsingh <chashmeetsingh@gmail.com>
2014-12-04 00:49:27 +0530

    Update SKYLINE.py

--- SKYLINE.py
+++ SKYLINE.py
@@ -1,8 +1,8 @@
-ar = []
-ar.append(1)
+a = []
+a.append(1)
 for i in range(1, 1001):
-	ar.append(ar[i-1]*(4*i-2)/(i+1))
+	a.append ( a [ i - 1 ] * (4 * i - 2 ) / ( i + 1 ) )
 n = input()
 while(n>0):
-	print ar[n]%1000000
+	print ar[n] % 1000000
 	n = input()
